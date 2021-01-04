5281d4a9a7f17a16987a948789df7b554e697384
catprice444 <catprice444@gmail.com>
2021-01-04 18:45:13 -0500

    save

--- app/controllers/workday_controller.rb
+++ app/controllers/workday_controller.rb
@@ -1,47 +1,47 @@
 class WorkdayController < ApplicationController
 
-    get "/workdays" do 
+    get '/workdays' do 
         if logged_in?
             @workdays = Workday.all
-            erb :'workdays/index'
+            erb :'/workdays/index'
         else 
-            redirect to "/login"
+            redirect '/login'
         end 
     end 
 
-    get "/workdays/new" do 
+    get '/workdays/new' do 
         if logged_in?
-            erb :'workdays/new'
+            erb :'/workdays/new'
         else 
-            redirect to "/login"
+            redirect '/login'
         end 
     end 
 
-    post "/workdays" do 
+    post '/workdays' do 
         if current_user.nil?
             flash[:error] = "Please login or sign up"
-            redirect to "/login"
+            redirect '/login'
 
         elseif params[:shift_start] == "" || params[:shift_start_time] == "" || params[:shift_end] == "" || params[:shift_end_time] == ""
             flash[:error] = "Fill in all fields to submit form"
-            redirect to "/workdays/new"
+            redirect '/workdays/new'
 
         else 
             @workday = Workday.new(:shift_start => params[:shift_start], :shift_end => params[:shift_end], 
                 :shift_start_time => params[:shift_start_time], :shift_end_time => params[:shift_end_time], :notes => params[:notes])
             @workday.user_id = current_user.id
             @workday.save
-            redirect to "/workdays/#{@workday.id}"
+            redirect "/workdays/#{@workday.id}"
         end 
-        redirect to "/workdays"
+        redirect '/workdays'
     end 
 
     get '/workdays/:id' do 
         if logged_in?
             @workday = Workday.find_by_id(params[:id])
-            erb :'workdays/show'
+            erb :'/workdays/show'
         else 
-            redirect to "/login"
+            redirect '/login'
         end 
     end 
 
@@ -50,23 +50,23 @@ class WorkdayController < ApplicationController
             @workday = Workday.find_by_id(params[:id])
 
             if @workday && (@workday.user_id == current_user.id)
-                erb :'workdays/edit'
+                erb :'/workdays/edit'
             else 
                 flash[:error] = "You don't have permission to edit this workday"
-                redirect to "/workdays"
+                redirect '/workdays'
             end 
 
         else 
-            redirect to "/login"
+            redirect '/login'
         end 
     end 
 
     patch '/workdays/:id' do 
         if logged_in?
             
-            if params[:shift_start] == "" || params[:shift_start_time] == "" || params[:shift_end] == "" || params[:shift_end_time] == ""
+            if params[:shift_start] == "" || params[:shift_start_time] == "" || params[:shift_end] == "" || params[:shift_end_time] == "" || params[:notes] == ""
                 flash[:error] = "All fields need information"
-                redirect to "/workdays/#{params[:id]}/edit"
+                redirect "/workdays/#{params[:id]}/edit"
                 
 
             else 
@@ -81,33 +81,33 @@ class WorkdayController < ApplicationController
                     @workday.notes = params[:notes]
                     @workday.save
 
-                    redirect to "/workdays/#{@workday.id}"
+                    redirect "/workdays/#{@workday.id}"
 
                 else 
                     flash[:error] = "You don't have permission to update this workday"
-                    redirect to "/workdays"
+                    redirect "/workdays"
 
                 end 
             end 
 
         else 
-            redirect to "/login"
+            redirect "/login"
         end             
     end 
 
-    delete "/workdays/:id/delete" do 
+    delete '/workdays/:id' do 
         if logged_in?
             @workday = Workday.find_by_id(params[:id])
             if @workday && (@workday.user_id == current_user.id)
                 @workday.delete
                 flash[:success] = "You successfully deleted the workday"
-                redirect to "/workdays"
+                redirect "/workdays"
             else 
                 flash[:error] = "You don't have permission to delete this workday"
-                redirect to "/workdays"
+                redirect "/workdays"
             end 
         else 
-            redirect to "/login"
+            redirect "/login"
         end 
     end 
 
